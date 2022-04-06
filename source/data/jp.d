513b91213dbe64cad4c9f0cd1532d36fa315f0ab
ishax-kos <isaacbunsen@gmail.com>
2022-04-06 08:09:10 -0700

    Move table descriptions for jp game to data.jp

--- source/data/jp.d
+++ source/data/jp.d
@@ -109,3 +109,22 @@ int mapping_0C = glyphs2x2.countUntil("Ⓐ");
 
 immutable String_t[86] windHylian;
 
+
+string[] tableDescriptions = [
+    /// 0
+    "Save, quit, and sleep menus.",
+    /// 1
+    "Game credits.",
+    "Names of NPCs.",
+    "Hints. Mostly from Tessin.",
+    "Item Names.",
+    /// 4
+    "Item get messages, and signs.",
+    "Signposts.",
+    "Place names.",
+    "Figurine titles.",
+    /// 8
+    "Figurine descriptions.",
+    "Debug messages.",
+    ""
+];
